d54d8f60e28027119fe4cdf9f498b29cfefae886
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2024-12-14 14:06:25 +0100

    Add 3 different datasets and use them for the performance analysis.

--- src/main.py
+++ src/main.py
@@ -3,21 +3,25 @@
 from performance_tools import timeit_analysis, cprofile_analysis, memory_analysis, pyinstrument_analysis
 from utils import generate_random_list
 
-SIZE = 1000
-
 if __name__ == "__main__":
     print("Performance Tests with Sorting Algorithms")
 
-    data = generate_random_list(SIZE)
+    hundred_records = generate_random_list(100)
+    thousand_records = generate_random_list(1000)
+    ten_thousand_records = generate_random_list(10000)
 
     # Timeit-Analyse
-    timeit_analysis(data)
+    # timeit_analysis(hundred_records)
+    # timeit_analysis(thousand_records)
+    timeit_analysis(ten_thousand_records)
 
     # cProfile-Analyse
-    cprofile_analysis(data)
+    cprofile_analysis(ten_thousand_records)
 
     # Memory Profiler-Analyse
-    memory_analysis(data)
+    memory_analysis(ten_thousand_records)
 
     # Pyinstrument-Analyse
-    pyinstrument_analysis(data)
+    pyinstrument_analysis(thousand_records)
+    # pyinstrument_analysis(hundred_records)
+    # pyinstrument_analysis(ten_thousand_records)
